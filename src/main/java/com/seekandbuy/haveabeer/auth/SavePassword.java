bc98b00257149f4f18eeb4e4872e13726a4f04e2
jhon3 <jhonattan.yoru@gmail.com>
2018-10-07 15:06:38 -0300

    tokenizando password

---
+++ src/main/java/com/seekandbuy/haveabeer/auth/SavePassword.java
@@ -0,0 +1,45 @@
+package com.seekandbuy.haveabeer.auth;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.seekandbuy.haveabeer.dao.UserDao;
+import com.seekandbuy.haveabeer.domain.User;
+import com.seekandbuy.haveabeer.exceptions.PromotionNotFoundException;
+import com.seekandbuy.haveabeer.exceptions.TokenNotFoundException;
+
+public class SavePassword {
+	private UserDao userDao;
+	private Map<String, Object> codification;
+	
+	public SavePassword(){
+		codification = new HashMap<String, Object>();
+	}
+	
+	public String tokenizerPassword(String password) {
+		UUID uuid = UUID.randomUUID();
+		String myRandom = uuid.toString();
+		boolean validToken = false;
+		
+		String token = null;
+		while(!validToken) {
+			token = myRandom.substring(0,20); //gera uma chave de token
+			if(!codification.containsKey(token)) { //se essa chave nao estiver na hash, adiciona ligada o password informado
+				validToken = true;
+			}
+		}
+		codification.put(password, token);
+		return token;
+	}
+	
+	public String findTokenByPassword(String password){
+		
+		String token = (String) codification.get(password);
+		if(token == null) {
+			throw new TokenNotFoundException("Token can not be found. Please, put the correct password!");
+		}
+		return token;
+		
+	}
+}
